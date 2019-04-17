065d1ae448e34c99da636f45d28162bd6466a837
Ali Baraka <abaraka@Alis-MacBook-Air.local>
2019-04-17 15:53:13 -0500

    pushing to master

--- main.js
+++ main.js
@@ -95,19 +95,3 @@ searchBox.addEventListener('keyup', searchCoffees);
 
 var submitButton = document.querySelector('#submit');
 submitButton.addEventListener('click', updateCoffees);
-
-
-//target the #search-coffee-name input to confirm name with array
-var checkName = document.getElementById('search-coffee-name');
-
-// //addEventListener to button
-// changeButton.addEventListener('click', changeColor, false);
-
-
-//another example of updating values with event listeners
-var newInstructor = document.getElementById('add');
-
-var conName = function(event){
-    var instructor = document.getElementById('5');
-    instructor.innerText = newInstructor.value;
-};
